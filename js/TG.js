fd62f49c9d42f559860d357a291e9b3f6e35fede
GG130 <98255176+BogdanVladimirov@users.noreply.github.com>
2023-09-10 10:01:51 +0300

    Create TG.js

---
+++ js/TG.js
@@ -0,0 +1,19 @@
+let Films = {
+    "Barbie": {
+        "years": 2023,
+        "contry": ["USA", "Great Britan"],
+        "genre": ["comedy", "adventure", "fantasy"]
+    },
+
+    "Oppenheimer": {
+        "years": 2023,
+        "contry": ["USA", "Great Britan"],
+        "genre": ["drama", "biography", "history"]
+    },
+
+    "Spider-Man: Across the Spider-Verse": {
+        "years": 2023,
+        "contry": ["USA"],
+        "genre": ["cartoon", "science", "fiction", "fantasy", "action", "adventure"]
+    },
+};
