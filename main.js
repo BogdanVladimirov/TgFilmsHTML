524fae0e2663e4f27c9afa96e22574bcd586a44d
GG130 <98255176+BogdanVladimirov@users.noreply.github.com>
2023-09-20 19:44:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const TelegramApi = require("node-telegram-bot-api");
 const token = "6158871311:AAGRPEkXEOgoZKKIYoSWpXLwPX-uQ2GajTU";
+const WebAppURL = "https://bogdanvladimirov.github.io/TgFilmsHTML/";
 const bot = new TelegramApi(token, {polling: true});
 
 bot.on("message", msg =>
@@ -7,9 +8,14 @@ bot.on("message", msg =>
     const ChatID = msg.chat.id;
     if(msg.text == "/start")
     {
-        // bot.sendMessage(ChatID, `Здравствуйте,  ${msg.from.first_name}, Это сообщение в боте специально для вас, вы не можете это увидеть, но знайте. Серверная часть этого бота написана БЕЗ ЕДИНОЙ МЫСЛИ О ГАДЮГЕ 3.0. тут используется только ЧИСТЫЙ JS, хоть и с небольшими загруженными модулями. А что бы вы в этом окончательно убедились - примите в дар мой код;`);
-        // setTimeout(() => {bot.sendMessage(ChatID, "Для запуска скрипта наипшите в терминале 'npm run dev'...")}, 100);
-        // bot.sendDocument(ChatID, 'MessageScript/main.js');
-        
+        bot.sendMessage(ChatID, 'Категории фильмов', {
+            reply_markup: {
+                inline_keyboard:[
+                    [{text: 'Открыть окно', web_app: {url: WebAppURL}}]
+                ]
+            }
+
+        });
+
     }
 })
