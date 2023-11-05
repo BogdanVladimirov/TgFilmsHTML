d1ccfadd16cfe369568717033c654e7b5b059d95
GG130 <98255176+BogdanVladimirov@users.noreply.github.com>
2023-11-05 10:26:41 +0300

    Update app.js

--- Database/app.js
+++ Database/app.js
@@ -1,20 +1,51 @@
 const fs = require('fs');
 
-const array = [""];
-
 fs.readFile('document.json', 'utf8', function(error, data) {
     if (error) throw error;
     //console.log(data);
     result = JSON.parse(data);
     const names = Object.keys(result);
     const years = [];
-    for (let name of names) {
-        if (!years.includes(result[name]['year'])) {
-            years.push(result[name]['year']);
+    function YearOfTheFilms()
+    {
+        for (let name of names) {
+            if (!years.includes(result[name]['year'])) {
+                years.push(result[name]['year']);
+            }
         }
+        years.sort();
+        console.log(years);
     }
-    years.sort();
-    console.log(years);
-
-
+    
+    const countries = [];
+    function CountryOfTheFilms()
+    {
+        for (let name of names) {
+            for(let index of result[name]['country'])
+            {
+                if (!countries.includes(index)) {
+                    countries.push(index);
+                }
+            }
+        }
+        countries.sort();
+        console.log(countries);
+    }
+    
+    const genres = [];
+    function GenreOfTheFilms()
+    {
+        for (let name of names) {
+            for(let index of result[name]['genre']){
+                if (!genres.includes(index)) {
+                    genres.push(index);
+                }
+            }
+        }
+        genres.sort();
+        console.log(genres);
+    }
+    YearOfTheFilms();
+    CountryOfTheFilms();
+    GenreOfTheFilms();
 });
